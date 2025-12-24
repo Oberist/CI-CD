b98b95a0596cbaebf8f08ab6137a3e1026eac6a7
Oberist <newcasty@mail.ru>
2025-12-24 23:07:56 +0300

    Fix test to match API response

--- test_main.py
+++ test_main.py
@@ -6,4 +6,4 @@ client = TestClient(app)
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello from CI/CD!"}
+    assert response.json() == {"message": "Hello CI/CD with FastAPI"}
